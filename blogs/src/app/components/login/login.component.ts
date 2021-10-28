aae18e121e51e25f3fda3a2fb3cd6c165f8cd4e0
Albert Wan <43050666+bertw2002@users.noreply.github.com>
2021-10-27 23:04:36 -0500

    refinements

--- blogs/src/app/components/login/login.component.ts
+++ blogs/src/app/components/login/login.component.ts
@@ -30,7 +30,7 @@ export class LoginComponent implements OnInit {
 
     ngOnInit(): void {
         if (this.authService.userLoggedIn) {                       // if the user's logged in, navigate them to the dashboard (NOTE: don't use afAuth.currentUser -- it's never null)
-            this.router.navigate(['/dashboard']);
+            this.router.navigate(['/home']);
         }
     }
 
@@ -44,7 +44,7 @@ export class LoginComponent implements OnInit {
             this.isProgressVisible = false;                     // no matter what, when the auth service returns, we hide the progress indicator
             if (result == null) {                               // null is success, false means there was an error
                 console.log('logging in...');
-                this.router.navigate(['/dashboard']);                // when the user is logged in, navigate them to dashboard
+                this.router.navigate(['/home']);                // when the user is logged in, navigate them to dashboard
             }
             else if (result.isValid == false) {
                 console.log('login error', result);
